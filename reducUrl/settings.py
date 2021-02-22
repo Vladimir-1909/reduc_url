5f5088286bab626e45cb02d4ffc90a36ed134141
Vladimir Baranov <vtgk12ak@gmail.com>
2021-02-22 23:31:14 +0500

    Changes settings and Procfile

--- reducUrl/settings.py
+++ reducUrl/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +15,7 @@ SECRET_KEY = 'z)os*n6rf06(v%zaufj)#*pcf6mbsgi(*v4homhysjj*pyf&jk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -120,3 +121,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
 
 LOGIN_URL = 'user'
 LOGIN_REDIRECT_URL = 'profile'
+
+django_heroku.settings(locals())
